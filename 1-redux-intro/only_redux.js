3c50c37f8db4fb01428f16c1496527a13dc152a5
adithya-naik <jatothadithyanaik@gmail.com>
2024-12-03 11:30:00 +0530

    till life cycle of redux

---
+++ 1-redux-intro/only_redux.js
@@ -0,0 +1,65 @@
+
+// LIFE CYCLE OF REDUX is completly covered
+
+
+
+
+const redux = require("redux");
+
+const INITIAL_VALUE = {
+  counter: 0,
+};
+
+const reducer = (store = INITIAL_VALUE, action) => {
+  let newStore = store;
+  if (action.type === "INCREMENT") {
+    newStore = { counter: store.counter + 1 };
+  } else if (action.type === "DECREMENT") {
+    newStore = { counter: store.counter - 1 };
+  } else if (action.type === "ADDITION") {
+    newStore = { counter: store.counter + action.payload.number };
+  }
+  return newStore;
+};
+
+// initial value is passed in reducder function
+const store = redux.createStore(reducer);
+
+// sunscriibe just creared not called yet
+const subscriber = () => {
+  // gets data from store
+  const state = store.getState();
+  console.log(state);
+};
+// to call subscribe
+store.subscribe(subscriber);
+// this means this subscriber function subxscribed and whnen ever the data of subsriber changes this fucnrtion repaints
+
+// to create actions then we have to so it by dispacth
+
+store.dispatch({
+  type: "INCREMENT",
+});
+
+store.dispatch({
+  type: "DECREMENT",
+});
+
+store.dispatch({
+  type: "ADDITION",
+  payload: {
+    number: 7,
+  },
+});
+
+store.dispatch({
+  type: "INCREMENT",
+});
+
+store.dispatch({
+  type: "DECREMENT",
+});
+
+store.dispatch({
+  type: "INCREMENT",
+});
